7db5910c84578c837faf83c402740aec41c8829c
HermannChoi <hiyunseok347@gmail.com>
2023-12-14 10:07:54 +0900

    [ADD]sidebarMenuAdded

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import styles from "./Sidebar.module.scss";
 import Profile from "./Profile/Profile";
+import Image from "next/image";
+import markup from "/public/images/markup.svg";
+import html from "/public/images/html.svg";
+import markdown from "/public/images/markdown.svg";
+import palette from "/public/images/palette.svg";
+import css from "/public/images/css.svg";
+import sass from "/public/images/sass.svg";
+import language from "/public/images/language.svg";
+import js from "/public/images/js.svg";
+import ts from "/public/images/ts.svg";
+import library from "/public/images/library.svg";
+import nodejs from "/public/images/nodejs.svg";
+import nextjs from "/public/images/nextjs.svg";
+import spring from "/public/images/language.svg";
 
 const Sidebar = () => {
   return (
@@ -8,18 +22,96 @@ const Sidebar = () => {
       <Profile />
       <div className={styles.sidebar_inner}>
         <div className={styles.root}>ROOT(0)</div>
-        <div className={styles.programming}>
-          ...Programming
-          <div className={styles.in_programming}>Mark Up</div>
-          <div className={styles.in_programming}>Style Sheet</div>
-          <div className={styles.in_programming}>Language</div>
-          <div className={styles.in_programming}>Library</div>
+        <div className={styles.st_menu}>
+          <div className={styles.st_name}> ...Programming</div>
+          <div className={styles.nd_menu}>
+            <div className={styles.nd_name_space}>
+              <Image src={markup} alt="mu" className={styles.icons} />
+              <span>Mark Up</span>
+            </div>
+            <div className={styles.rd_menu}>
+              <div className={styles.rd_name_space}>
+                <span> {`>`} </span>
+                <Image src={html} alt="html" className={styles.icons2} />
+                <span>HTML</span>
+              </div>
+            </div>
+            <div className={styles.rd_menu}>
+              <div className={styles.rd_name_space}>
+                <span> {`>`} </span>
+                <Image src={markdown} alt="html" className={styles.icons2} />
+                <span>MarkDown</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.nd_menu}>
+            <div className={styles.nd_name_space}>
+              <Image src={palette} alt="mu" className={styles.icons} />
+              <span>Style Sheet</span>
+            </div>
+            <div className={styles.rd_menu}>
+              <div className={styles.rd_name_space}>
+                <span> {`>`} </span>
+                <Image src={css} alt="html" className={styles.icons2} />
+                <span>CSS</span>
+              </div>
+            </div>
+            <div className={styles.rd_menu}>
+              <div className={styles.rd_name_space}>
+                <span> {`>`} </span>
+                <Image src={sass} alt="html" className={styles.icons2} />
+                <span>SCSS</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.nd_menu}>
+            {" "}
+            <div className={styles.nd_name_space}>
+              <Image src={language} alt="mu" className={styles.icons} />
+              <span>Language</span>
+            </div>
+            <div className={styles.rd_menu}>
+              <div className={styles.rd_name_space}>
+                <span> {`>`} </span>
+                <Image src={js} alt="html" className={styles.icons2} />
+                <span>JavaScript</span>
+              </div>
+            </div>
+            <div className={styles.rd_menu}>
+              <div className={styles.rd_name_space}>
+                <span> {`>`} </span>
+                <Image src={ts} alt="html" className={styles.icons2} />
+                <span>TypeScript</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.nd_menu}>
+            {" "}
+            <div className={styles.nd_menu}>
+              {" "}
+              <div className={styles.nd_name_space}>
+                <Image src={library} alt="mu" className={styles.icons} />
+                <span>Library</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.framework}>
-          ...Framework
-          <div className={styles.in_framework}>Node.js</div>
-          <div className={styles.in_framework}>Next.js</div>
-          <div className={styles.in_framework}>Spring</div>
+        <div className={styles.st_menu}>
+          <div className={styles.st_name}> ...Framework</div>
+          <div className={styles.nd_menu}>
+            <div className={styles.nd_name_space}>
+              <Image src={nodejs} alt="mu" className={styles.icons} />
+              <span>Node.js</span>
+            </div>
+            <div className={styles.nd_name_space}>
+              <Image src={nextjs} alt="mu" className={styles.icons} />
+              <span>Next.js</span>
+            </div>
+            <div className={styles.nd_name_space}>
+              <Image src={spring} alt="mu" className={styles.icons} />
+              <span>Spring</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <div className={styles.body}>
-        <Header />a
+        <Header />
       </div>
     </>
   );
